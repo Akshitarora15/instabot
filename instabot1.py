9e9fe987573dee4f0e642ae3e4b139328d73c606
Akshit Arora <aroraakshit15@gmail.com>
2017-07-12 20:36:53 +0530

    some changes to the code

--- instabot1.py
+++ instabot1.py
@@ -1,12 +1,10 @@
 import requests, urllib
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
-from clarifai.rest import  ClarifaiApp
+import matplotlib.pyplot as plt
 
 APP_ACCESS_TOKEN = '1525979253.7d7ade8.a2d201f6ad2a4bbf9eded13b9094308f' #My own access token
-API_KEY ='8yuXGEPkygEXsta5zfGza8JkD9EqleC7guz2xmS2BEE' #key for parallel dots
 BASE_URL = 'https://api.instagram.com/v1/' #basic url of instagram
-BASE_URL_paralleldots='https://apis.paralleldots.com/'  #url of parallel dots
 
 def self_info():  #Getting our own details of instagram account
     request_url = (BASE_URL + 'users/self/?access_token=%s') %(APP_ACCESS_TOKEN)
@@ -152,49 +150,37 @@ def post_a_comment(insta_username):  #Commenting the recent post of user
     else:
         print "Unable to add comment. Try again!"
 
-def comment_analyse(insta_username):  #Analysing the comment based on sentiments
+def pie_chart(insta_username):
     media_id = get_post_id(insta_username)
-    payload = {"access_token": APP_ACCESS_TOKEN}
-    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
-    if media_id==None:
-        print "No media exist"
-        exit()
-        if media_id['data']['text']==None:
-            print "there is no comment to analyse"
-        else:
-            request_url = BASE_URL_paralleldots + 'sentiment?sentence1=%s&apikey=%s' % (input, API_KEY)
-            response = requests.get(request_url).json()
-            print response
-            if response > 0.7:
-                print "the response is positive"
-            else:
-                print "the response is negative"
+    comment_info = requests.get(request_url).json()
+    if comment_info['meta']['code'] == 200:
+        if len(comment_info['data']):
 
-def image_analyse(insta_username): #analyse the image based on sentiments
-    user_id=get_user_id(insta_username)
-    if user_id==None:
-        print "User does not exist"
-        exit()
-    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
-    user_media = requests.get(request_url).json()
+            for x in range(0, len(comment_info['data'])):
+                comment_id = comment_info['data'][x]['id']
+                comment_text = comment_info['data'][x]['text']
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                positive_comment = blob.sentiment.p_pos
+                negative_comment = blob.sentiment.p_neg
+                labels = 'Positive Comment', 'Negative Comment'
+                sizes = [positive_comment,negative_comment]
+                colors = [ 'blue', 'lightcoral',]
+                explode = ( 0, 0)
 
-    if user_media['meta']['code'] == 200:
-        if len(user_media['data']):
-            image_name = user_media['data'][0]['id'] + '.jpeg'
-            image_url=user_media['data'][0]['images']['standard_resolution']['url']
-            app = ClarifaiApp(api_key='f46c3d88d4a747598d8c109b244f366b')
-            model = app.models.get('general-v1.0')
-            response = model.predict_by_url('image-url=image_url')
-            input = response
-            request_url = BASE_URL_paralleldots + 'sentiment?sentence1=%s&apikey=%s' % (input, API_KEY)
-            response = requests.get(request_url).json()
-            print response
+                plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+                        autopct='%1.1f%%', shadow=True, startangle=140)
+
+                plt.axis('equal')
+                plt.show()
+
         else:
-            print "image does not exist"
+            print 'There are no existing comments on the post!'
     else:
-        print "status code other than 200"
+        print 'Status code other than 200 received!'
+
+
 
 def delete_negative_comment(insta_username):  #Deleting the negative comments
     media_id = get_post_id(insta_username)
@@ -239,9 +225,8 @@ def start_bot():  #Starting my own Instabot
         print "e.Like the recent post of a user"
         print "f.Make a comment on the recent post of a user"
         print "g.Delete negative comments from the recent post of a user"
-        print "h.Analyse a comment"
-        print "i.Analyse an image"
-        print "j.Exit"
+        print "h.Pie chart function for positive and negative comments"
+        print "i.Exit"
 
 
         choice = raw_input("Enter you choice: ")
@@ -266,11 +251,8 @@ def start_bot():  #Starting my own Instabot
            delete_negative_comment(insta_username)
         elif choice=="h":
             insta_username = raw_input("Enter the username of the user: ")
-            comment_analyse(insta_username)
-        elif choice=="i":
-            insta_username = raw_input("Enter the username of the user: ")
-            image_analyse(insta_username)
-        elif choice == "j":
+            pie_chart(insta_username)
+        elif choice == "i":
             exit()
         else:
             print "wrong choice"
